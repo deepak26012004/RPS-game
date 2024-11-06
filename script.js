e0fa39c8045ad0bb0ac54bd491fe3847248b5aad
deepak26012004 <dk7738224@gmail.com>
2024-11-06 21:35:06 +0530

    some small changes

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+let playerScore = 0;
+let computerScore = 0;
+const Rounds = 5;
+
 function playGame(playerChoice) //We create a function//
 {
 
     const userChoice = ['Rock', 'Paper', 'Scissors']; // We create user choice//
-    const computerChoice = userChoice[Math.floor(Math.random() * 3)];
+    const computerChoice = userChoice[Math.floor(Math.random() * userChoice.length)];
     //We get the random choice from computer //
     let result;
 
@@ -10,20 +14,20 @@ function playGame(playerChoice) //We create a function//
     if (playerChoice === computerChoice) {
 
         result = 'match is draw';
-
     } else
-    if ((playerChoice === 'Rock' && computerChoice == 'scissors') || //Rock > scissor = Rock winner(Rock brake the scissors)//
+    if ((playerChoice === 'Rock' && computerChoice == 'Scissors') || //Rock > scissor = Rock winner(Rock brake the scissors)//
 
-        (playerChoice === 'scissors' && computerChoice == 'Paper') || //scissors > Paper = scissors winner(scissors cut the paper)//
+        (playerChoice === 'Scissors' && computerChoice == 'Paper') || //scissors > Paper = scissors winner(scissors cut the paper)//
 
         (playerChoice === 'Paper' && computerChoice == 'Rock')) //paper > rock = paper winner(paper complete roll the rock)//
     {
 
         result = `You Win! ${playerChoice} beats ${computerChoice}`; // if player win the match//
-
+        playerScore++;
     } else {
 
-        result = `You Lose! ${computerChoice} beats ${playerChoice};` // if player lose the match//
+        result = `You Lose! ${computerChoice} beats ${playerChoice}`; // if player lose the match//
+        computerScore++;
     }
     document.getElementById("result").innerText = result;
 }
