fd1cd5d0b7acf1f3f97ae6604ed0e5dca2768c10
deepak26012004 <dk7738224@gmail.com>
2024-11-06 12:32:33 +0530

    updates

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function playGame(playerChoice) //We create a function//
 {
+
     const userChoice = ['Rock', 'Paper', 'Scissors']; // We create user choice//
-    const computerChoice = userChoice[Math.floor(Math.random() * 3)]; //We get the random choice from computer //
+    const computerChoice = userChoice[Math.floor(Math.random() * 3)];
+    //We get the random choice from computer //
     let result;
+
 
     if (playerChoice === computerChoice) {
 
         result = 'match is draw';
 
-    } else if ((playerChoice === 'Rock' && computerChoice == 'scissors') || //Rock > scissor = Rock winner(Rock brake the scissors)//
+    } else
+    if ((playerChoice === 'Rock' && computerChoice == 'scissors') || //Rock > scissor = Rock winner(Rock brake the scissors)//
 
         (playerChoice === 'scissors' && computerChoice == 'Paper') || //scissors > Paper = scissors winner(scissors cut the paper)//
 
@@ -18,8 +22,8 @@ function playGame(playerChoice) //We create a function//
         result = `You Win! ${playerChoice} beats ${computerChoice}`; // if player win the match//
 
     } else {
+
         result = `You Lose! ${computerChoice} beats ${playerChoice};` // if player lose the match//
     }
-    document.getElementById("result").innerHTML = result
-
+    document.getElementById("result").innerText = result;
 }
